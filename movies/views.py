74f3a9111aa004bd258dd3dfaab0bbc60eeb48f2
sydneyfriedman <friedman.sydneyl@gmail.com>
2025-01-11 19:19:31 -0500

    Created Django view to fetch and display movies, including information like release year, IMDb rating, genre, etc. Integrated Chart.js to create a graph of IMDb ratings over time.

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from .models import Movie
+from django.core.serializers import serialize
+import json
 
 def movie_list(request):
-    movies = Movie.objects.all()
-    return render(request, 'movies/movie_list.html', {'movies': movies})
+    # Fetch all movies ordered by release year
+    movies = Movie.objects.all().order_by('-released_year')
+
+    # Serialize the movie data to pass it to the template
+    movie_data = list(movies.values('released_year', 'imdb_rating'))
+
+    # Return the template with the context
+    return render(request, 'movies/movie_list.html', {
+        'movies': movies,
+        'movie_data': json.dumps(movie_data)  # Passing data as JSON string
+    })
